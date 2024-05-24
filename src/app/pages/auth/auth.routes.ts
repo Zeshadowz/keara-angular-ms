797050f6de2ad48224e7fa59481aca235c0bea5c
Kevin Azangue <kevinromuald@gmail.com>
2024-05-24 23:37:52 +0200

    some architectur added

---
+++ src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from "@angular/router";
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { ResetPasswordComponent } from "./reset-password/reset-password.component";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { LockScreenComponent } from "./lock-screen/lock-screen.component";
+
+export const authRoutes: Routes = [
+  {
+    path: 'login',
+    component: SignInComponent,
+    data: {title: 'Login'}
+  }, {
+    path: 'register',
+    component: SignUpComponent,
+    data: {title: 'Register'}
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+    data: {title: 'Reset Password'}
+  }, {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    data: {title: 'Forgot Password'}
+  }, {
+    path: 'lock-screen',
+    component: LockScreenComponent,
+    data: {title: 'Lock Screen'}
+  }
+]
